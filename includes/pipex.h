30209ce3a30899ff324cc13762612bf6c12d3083
user42 <nfauconn@student.42.fr>
2022-04-26 16:45:21 +0200

    rr

--- includes/pipex.h
+++ includes/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 18:25:20 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/04/22 18:49:27 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/04/26 12:19:50 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <unistd.h>
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
 
 #endif
--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 18:32:16 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/04/22 18:33:21 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/04/26 14:34:24 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 int	main(int ac, char **argv)
 {
-	if (ac == 2)
-		printf("%s\n", argv[1]);
+	errno = 0;
+	if (ac == 5)
+	{
+		open(argv[1], O_WRONLY, O_APPEND);
+		if (errno != 0)
+			printf("%s\n", strerror(errno));
+	}
+	else
+		write(STDERR, "wrong number of arguments\n", 27);
 	return (0);
 }
