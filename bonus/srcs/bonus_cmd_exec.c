9d53a1ea578076798f4ed1fa7cb1787553fb36d2
user42 <nfauconn@student.42.fr>
2022-06-04 14:59:44 +0200

    oOooOoo

--- bonus/srcs/bonus_cmd_exec.c
+++ bonus/srcs/bonus_cmd_exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bonus_cmd_exec.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 12:03:00 by nono              #+#    #+#             */
-/*   Updated: 2022/06/03 18:12:41 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/06/04 14:58:44 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,80 +29,86 @@ static int	find_path_then_execve(char **cmd, char **paths, char **env)
 	return (0);
 }
 
-static void	child_exec(t_data *data, t_cmd *cmd, int *write_fd, int *read_fd)
+static void	child_exec(t_data *data, t_cmd *cmd, int read_fd, int write_fd)
 {
-//	int		redir[2];
-
-	close(write_fd[0]);
-	close(read_fd[1]);
-/* 	redir[0] = data->fd_in;
-	redir[1] = pipe[1];*/
-	clean_dup(data, cmd->tab, read_fd[0], STDIN_FILENO); 
-	clean_dup(data, cmd->tab, write_fd[1], STDOUT_FILENO);
+	clean_dup(data, cmd->tab, read_fd, STDIN_FILENO);
+	clean_dup(data, cmd->tab, write_fd, STDOUT_FILENO);
 	if (!find_path_then_execve(cmd->tab, data->paths, data->env_tab))
 	{
-		close(read_fd[0]);
-		close(write_fd[1]);
-		error_exit(data, cmd->tab[0], NULL, "mid command not found");
-	}
-}
-
-static void	child_first_exec(t_data *data, t_cmd *cmd, int *write_fd, int *read_fd)
-{
-	close(read_fd[0]);
-	close(read_fd[1]);
-	close(write_fd[0]);
-	clean_dup(data, cmd->tab, data->fd_in, STDIN_FILENO);
-	clean_dup(data, cmd->tab, write_fd[1], STDOUT_FILENO);
-	if (!find_path_then_execve(cmd->tab, data->paths, data->env_tab))
-	{
-		error_exit(data, cmd->tab[0], NULL, "first command not found");
-	}
-}
-
-static void	child_last_exec(t_data *data, t_cmd *cmd, int *write_fd, int *read_fd)
-{
-	close(read_fd[1]);
-	close(write_fd[0]);
-	close(write_fd[1]);
-	clean_dup(data, cmd->tab, read_fd[0], STDIN_FILENO);
-	clean_dup(data, cmd->tab, data->fd_out, STDOUT_FILENO);
-	if (!find_path_then_execve(cmd->tab, data->paths, data->env_tab))
-	{
-		error_exit(data, cmd->tab[0], NULL, "last command not found");
+		error_exit(data, cmd->tab[0], NULL, "command not found");
 	}
 }
 
 int	exec_cmd(t_data *data)
 {
 	int		i;
-	int		parent_to_child[2];
-	int		child_to_parent[2];
+	int		j;
+	int		fd_in;
+	int		fd_out;
+	int		redir[data->nb_cmd][2];
 	pid_t	pid;
 	t_cmd	*tmp;
 
 	tmp = data->cmd;
 	i = 0;
-
+	while (i < data->nb_cmd)
+	{
+		clean_pipe_creation(data, redir[i]);
+		i++;
+	}
+	i = 0;
+	j = 0;
 	while (tmp && i < data->nb_cmd)
 	{
-		clean_pipe_creation(data, parent_to_child);
-		clean_pipe_creation(data, child_to_parent);
 		pid = fork();
 		if (pid < 0)
 			error_exit(data, "fork", NULL, strerror(errno));//return (errno); 
 		if (pid == 0)
 		{
 			if (i == 0)
-				child_first_exec(data, tmp, child_to_parent, parent_to_child);
-			else if (i == data->nb_cmd - 1 && i % 2)
-				child_last_exec(data, tmp, child_to_parent, parent_to_child);
-			else if (i == data->nb_cmd - 1 && !(i % 2))
-				child_last_exec(data, tmp, child_to_parent, parent_to_child);
-			else if (!(i % 2))
-				child_exec(data, tmp, child_to_parent, parent_to_child);
+			{
+				close(redir[0][0]);
+				while (redir[++i])
+				{
+					close(redir[i][0]);
+					close(redir[i][1]);
+				}
+				i = 0;
+				child_exec(data, tmp, data->fd_in, redir[i][1]);
+			}
+			else if (i != data->nb_cmd - 1)
+			{
+				close(redir[i][0]);
+				close(redir[i - 1][1]);
+				fd_in = redir[i - 1][0];
+				fd_out = redir[i][1];
+				j = i;
+				i = 0;
+				while (redir[i])
+				{
+					if (redir[i][0] != fd_in)
+						close(redir[i][0]);
+					if (redir[i][1] != fd_out)
+						close(redir[i][1]);
+					i++;
+				}
+				i = j;
+				child_exec(data, tmp, redir[i - 1][0], redir[i][1]);
+			}
 			else
-				child_exec(data, tmp, parent_to_child, child_to_parent);
+			{
+				close(redir[i][1]);
+				fd_in = redir[i][0];
+				j = i;
+				i = 0;
+				while (redir[i])
+				{
+					if (redir[i][0] != fd_in)
+						close(redir[i][0]);
+					close(redir[i][1]);
+				}
+				child_exec(data, tmp, redir[i][0], data->fd_out);
+			}
 			break ;
 		}
 		else
